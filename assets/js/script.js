0f24a33ed966584de5232c528130ffd8ef0e1d42
Wahyu Wijiyanto <wejheyanto95@gmail.com>
2022-10-21 02:01:41 +0700

    add some content to <main>

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const disclaimerMessage = "DISCLAIMER : This website is still under development. If you choose to continue, don't be surprised that you may found some bugs and malfunctions, ESPECIALLY if you use your mobile phone :)"
+
+document.addEventListener('DOMContentLoaded', function () {
+    alert(disclaimerMessage)
+});
