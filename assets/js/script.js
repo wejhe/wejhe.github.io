7ae7db2b8c6eedcb6e4011ca16920205a051f455
Wahyu Wijiyanto <wejheyanto95@gmail.com>
2022-10-21 02:20:08 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 const disclaimerMessage = "DISCLAIMER : This website is still under development. If you choose to continue, don't be surprised that you may found some bugs and malfunctions, ESPECIALLY if you use your mobile phone :)"
 
-document.addEventListener('DOMContentLoaded', function () {
+window.addEventListener("load", function () {
     alert(disclaimerMessage)
 });
