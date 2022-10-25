91edc9c81be036949d14aa2b1535b22cade27e11
Wahyu Wijiyanto <wejheyanto95@gmail.com>
2022-10-25 17:37:40 +0700

    add hover effect to #main-circle

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,24 @@
-const disclaimerMessage = "DISCLAIMER : This website is still under development. If you choose to continue, don't be surprised that you may found some bugs and malfunctions, ESPECIALLY if you use your mobile phone :)"
+const main = document.getElementById("main");
+const mainCircle = document.getElementById("main-circle");
+const circleHint = document.getElementById("circle-hint");
+const mainTop = document.getElementById("main-top");
 
-/*document.addEventListener("DOMContentLoaded", function () {
-    alert(disclaimerMessage);
-});*/
+mainCircle.addEventListener("mouseover", function () {
+    mainCircle.style.width = "170px";
+    mainCircle.style.height = "170px";
+    mainCircle.style.borderRadius = "85px";
+    mainCircle.style.marginBottom = "30px";
+    main.style.paddingTop = "50px";
+    circleHint.style.left = "calc(50% + 90px)";
+    /*mainTop.style.visibility = "hidden";*/
+});
+
+mainCircle.addEventListener("mouseout", function () {
+    mainCircle.style.width = "150px";
+    mainCircle.style.height = "150px";
+    mainCircle.style.borderRadius = "75px";
+    mainCircle.style.marginBottom = "40px";
+    main.style.paddingTop = "60px";
+    circleHint.style.left = "calc(50% + 80px)";
+    /*mainTop.style.visibility = "visible";*/
+});
